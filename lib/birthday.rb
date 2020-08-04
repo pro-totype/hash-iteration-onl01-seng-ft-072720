bb267bf48311c4280afac54f80c913454442d38e
Sean B <butts.sean01@gmail.com>
2020-08-04 04:30:44 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,10 +12,11 @@ end
 
 
 def age_appropriate_birthday(birthday_kids)
-  birthday_kids.each do |key,value|
-  if value <= 12
-    puts "Happy Birthday #{key}! You are now #{value} years old!"
-  else
-    puts "You are too old for this."
+    birthday_kids.each do |key,value|
+      if value <= 12
+      puts "Happy Birthday #{key}! You are now #{value} years old!"
+      else
+      puts "You are too old for this."
+      end
   end
 end
